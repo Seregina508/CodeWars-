77dfeaded9f3e1e16e2b80d4d0365b48b5e24da5
Seregina508 <elenazav@mail.ru>
2020-05-05 00:11:15 -0400

    CodePractice

---
+++ AreaOfASquare.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5748838ce2fab90b86001b1a
+
+function squareArea(A){
+    let a = (2 * A) / Math.PI;
+    return  +(a * a).toFixed(2);
+}
---
+++ FormattingDecimalPlaces.js
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/5641a03210e973055a00000d
+function twoDecimalPlaces(n) {
+    return +n.toFixed(2);
+}
